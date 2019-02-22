ec1057f7c878affcbc23ef690ccb1eecbd339cfe
siyuan.lai <siyuan.lai@wenba100.com>
2019-02-23 00:17:52 +0800

    初始化

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        Model::unguard();
+
+        //$this->call(UsersTableSeeder::class);
+
+        Model::reguard();
+    }
+}
