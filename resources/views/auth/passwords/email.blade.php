96878c14a88385838e04665941b4a6b33505471b
siyuan.lai <siyuan.lai@wenba100.com>
2019-03-20 22:23:04 +0800

    找回密码成功消息提示

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,41 +1,47 @@
-@extends('layouts.default')
-@section('title', '重置密码')
+@extends('layouts.app')
 
 @section('content')
-    <div class="col-md-8 offset-md-2">
-        <div class="card">
-            <div class="card-header"><h5>重置密码</h5></div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Reset Password') }}</div>
 
-            <div class="card-body">
-                @if(session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-                <form action="{{ route('password.email') }}" method="POST" class="">
-                    {{ csrf_field() }}
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
 
-                    <div class="form-group{{ $errors->has('email')? ' has-error ' : '' }}">
-                        <label for="email" class="form-control-label">邮箱地址：</label>
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                        <input type="email" id="email" class="form-control" name="email" value="{{ old('email') }}"
-                               required>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
-                        @if($errors->has('email'))
-                            <span class="form-text">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
 
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary">
-                            发送密码重置邮件
-                        </button>
-                    </div>
-                </form>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Send Password Reset Link') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-
     </div>
+</div>
+@endsection
