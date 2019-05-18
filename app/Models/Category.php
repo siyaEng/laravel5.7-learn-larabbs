596b43eebba36b636160b5d3b6d284ad3039dbf4
siyuan.lai <siyuan.lai@wenba100.com>
2019-05-18 17:43:38 +0800

    话题顺序

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,14 @@ class Category extends Model
     protected $fillable = [
         'name', 'description',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
